10324144606d8ac18d8ac1c416bdf3111f35fe96
Julian Heaney <julianpheaney@gmail.com>
2022-09-04 16:33:23 -0500

    backend api requests

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,15 +3,17 @@ const sequelize = require('../../config/connection');
 const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-
+// get all users
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
         attributes: [
             'id',
+            // 'post_url',
             'title',
             'description',
-            'created_at'
+            'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
             {
@@ -42,9 +44,11 @@ router.get('/:id', (req, res) => {
         },
         attributes: [
             'id',
+            // 'post_url',
             'title',
             'description',
             'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [
             {
@@ -75,10 +79,11 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', withAuth, (req, res) => {
-    
+    // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
         title: req.body.title,
         description: req.body.description,
+        // post_url: req.body.post_url,
         user_id: req.session.user_id
     })
         .then(dbPostData => res.json(dbPostData))
@@ -88,11 +93,20 @@ router.post('/', withAuth, (req, res) => {
         });
 });
 
+// router.put('/upvote', withAuth, (req, res) => {
+//     // custom static method created in models/Post.js
+//     Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+//         .then(updatedVoteData => res.json(updatedVoteData))
+//         .catch(err => {
+//             console.log(err);
+//             res.status(500).json(err);
+//         });
+// });
+
 router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
-            title: req.body.title,
-            description: req.body.description
+            title: req.body.title
         },
         {
             where: {
